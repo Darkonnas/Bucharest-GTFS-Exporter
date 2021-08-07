53b73db7b5b4adf6de6f3273fda08136f7277614
Darkonnas <vlad.stoleru@gmail.com>
2021-08-07 20:00:01 +0300

    Add request retries when calling STB SA API

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-var fs = require('fs'),
+const fs = require('fs'),
     polyline = require('polyline-encoded'),
     request = require('sync-request');
 
 const util = require('util')
 
-var apiURL = "https://info.stbsa.ro",
-    hasMetroLines = true;
+const apiURL = "https://info.stbsa.ro", hasMetroLines = true;
 
-var currentService="LV";
+const currentService = "LV";
 
-var start = new Date();
+const start = new Date();
 
-var data = {
+let data = {
     "agency": [
         "agency_id,agency_name,agency_url,agency_timezone,agency_lang,agency_phone,agency_fare_url,agency_email",
         "1,STB SA,https://stbsa.ro,Europe/Bucharest,ro,0213110595,http://stbsa.ro/portofel_electronic.php,contact@stbsa.ro",
-        "2,METROREX SA,http://www.metrorex.ro/,Europe/Bucharest,0213193601,http://www.metrorex.ro/titluri_de_calatorie_p1381-1,contact@metrorex.ro"
+        "2,METROREX SA,http://www.metrorex.ro/,Europe/Bucharest,ro,0213193601,http://www.metrorex.ro/titluri_de_calatorie_p1381-1,contact@metrorex.ro"
     ],
     "stops": [
         "stop_id,stop_name,stop_desc,stop_lat,stop_lon"
@@ -42,18 +41,18 @@ var data = {
     ]
 };
 
-function randomIntFromInterval(min, max) { // min and max included 
+function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function blockWait() {
-    var waitTill = new Date(new Date().getTime() + randomIntFromInterval(1200, 3100));
+    const waitTill = new Date(new Date().getTime() + randomIntFromInterval(1200, 3100));
     while (waitTill > new Date()) {}
 }
 
 function zip(arrays) {
-    if (arrays && arrays[0]) return arrays[0].map(function(_, i) {
-        return arrays.map(function(array) {
+    if (arrays && arrays[0]) return arrays[0].map(function (_, i) {
+        return arrays.map(function (array) {
             return array[i]
         })
     });
@@ -61,14 +60,14 @@ function zip(arrays) {
 }
 
 function stopInList(id) {
-    for (var i = 0; i < data.stops.length; i++) {
+    for (let i = 0; i < data.stops.length; i++) {
         if (data.stops[i].split(',')[0] == id) return true;
     }
     return false;
 }
 
 function agencyInList(id) {
-    for (var i = 0; i < data.agency.length; i++) {
+    for (let i = 0; i < data.agency.length; i++) {
         if (data.agency[i].split(',')[0] == id) return true;
     }
     return false;
@@ -77,42 +76,54 @@ function agencyInList(id) {
 function processLineDirection(res2, id, dir, hs, sn) {
     blockWait();
     console.log("Line, direction: " + id + ", " + dir + " - " + hs);
-    var temp = [],
-        seqs = [];
-    if (res2.stops)
-        for (var i = 0; i < res2.stops.length; i++) {
-            if (!stopInList(res2.stops[i].id)) data.stops.push(res2.stops[i].id + ',"' + res2.stops[i].name + '","' + res2.stops[i].description + '",' + res2.stops[i].lat + "," + res2.stops[i].lng);
 
-            try {
-                res = request('GET', apiURL + '/rp/api/lines/' + id + '/stops/' + res2.stops[i].id);
-            } catch (err) {
-                blockWait();
-                processLineDirection(res2, id, dir, hs, sn);
+    let temp = [], seqs = [];
+
+    if (res2.stops) {
+        for (let i = 0; i < res2.stops.length; i++) {
+            if (!stopInList(res2.stops[i].id)) {
+                data.stops.push(res2.stops[i].id + ',"' + res2.stops[i].name + '","' + res2.stops[i].description + '",' + res2.stops[i].lat + "," + res2.stops[i].lng);
             }
-            res = JSON.parse(res.getBody('utf8'));
+
+            let res;
+            let repeat = true;
+
+            while (repeat) {
+                repeat = false;
+                try {
+                    res = request('GET', apiURL + '/rp/api/lines/' + id + '/stops/' + res2.stops[i].id);
+                    res = JSON.parse(res.getBody('utf8'));
+                } catch (err) {
+                    blockWait();
+                    repeat = true;
+                }
+            }
 
             if (res[0].lines[0] && res[0].lines[0].timetable)
-                for (var j = 0; j < res[0].lines[0].timetable.length; j++) {
-                    for (var k = 0; k < res[0].lines[0].timetable[j].minutes.length; k++) {
+                for (let j = 0; j < res[0].lines[0].timetable.length; j++) {
+                    for (let k = 0; k < res[0].lines[0].timetable[j].minutes.length; k++) {
                         temp.push("," + res[0].lines[0].timetable[j].hour + ":" + res[0].lines[0].timetable[j].minutes[k] + ":00," + res[0].lines[0].timetable[j].hour + ":" + res[0].lines[0].timetable[j].minutes[k] + ":30" + "," + res2.stops[i].id + "," + i);
                     }
                 }
         }
+    }
 
-    for (var i = 0; i < temp.length; i++) {
+    for (let i = 0; i < temp.length; i++) {
         if (!seqs[temp[i].split(',')[4]]) seqs[temp[i].split(',')[4]] = [temp[i]];
         else seqs[temp[i].split(',')[4]].push(temp[i]);
     }
 
     temp = zip(seqs);
 
-    if (temp)
-        for (var i = 0; i < temp.length; i++) {
-            data.trips.push(id + ","+currentService+",TRIP"+ currentService + id + dir + i + ',"' + hs + '",' + dir + ',' + id + dir);
-            var last = '';
-            var max = (hasMetroLines && sn.indexOf("M") != -1) ? (temp[i].length) : (temp[i].length - 1);
-            if (temp[i])
-                for (var j = 0; j < max; j++) {
+    if (temp) {
+        for (let i = 0; i < temp.length; i++) {
+            data.trips.push(id + "," + currentService + "," + `TRIP_${currentService}_${id}_${dir}_${i}` + ',"' + hs + '",' + dir + ',' + id + dir);
+
+            let last = '';
+            const max = (hasMetroLines && sn.indexOf("M") !== -1) ? (temp[i].length) : (temp[i].length - 1);
+
+            if (temp[i]) {
+                for (let j = 0; j < max; j++) {
                     if (temp[i][j]) {
                         linex = temp[i][j].split(',');
                         lline = last.split(',');
@@ -142,17 +153,18 @@ function processLineDirection(res2, id, dir, hs, sn) {
 
                         }
                         last = temp[i][j];
-                        data.stop_times.push("TRIP"+ currentService + id + dir + i + temp[i][j]);
+                        data.stop_times.push(`TRIP_${currentService}_${id}_${dir}_${i}` + temp[i][j]);
                     }
                 }
+            }
         }
+    }
 }
 
 function addShapes(linestring, id) {
+    let line = polyline.decode(linestring);
 
-    line = polyline.decode(linestring);
-
-    for (var i = 0; i < line.length; i++) {
+    for (let i = 0; i < line.length; i++) {
         data.shapes.push(id + "," + line[i][0] + "," + line[i][1] + "," + i);
     }
 
@@ -161,29 +173,37 @@ function addShapes(linestring, id) {
 function processLine(id) {
     console.log("Line: " + id);
 
-    try {
-        res = request('GET', apiURL + '/rp/api/lines/' + id + '/direction/0');
-        res = JSON.parse(res.getBody('utf8'));
-    } catch (err) {
-        blockWait();
-        processLine(id);
+    let res;
+    let repeat = true;
+
+    while (repeat) {
+        repeat = false;
+        try {
+            res = request('GET', apiURL + '/rp/api/lines/' + id + '/direction/0');
+            res = JSON.parse(res.getBody('utf8'));
+        } catch (err) {
+            blockWait();
+            repeat = true;
+        }
     }
+
+    let type;
 
     switch (res.type) {
         case "TRAM":
-            var type = 0;
+            type = 0;
             break;
         case "BUS":
-            var type = 3;
+            type = 3;
             break;
         case "CABLE_CAR":
-            var type = 11;
+            type = 11;
             break;
         case "SUBWAY":
-            var type = 1;
+            type = 1;
             break;
         default:
-            var type = 3;
+            type = 3;
     }
 
     data.routes.push(res.id + "," + res.organization.id + "," + res.name + "," + type + "," + res.color.replace("#", "") + ",ffffff,");
@@ -191,13 +211,17 @@ function processLine(id) {
     addShapes(res.segment_path, id + "0");
     processLineDirection(res, id, 0, res.direction_name_tur, res.name);
 
-    try {
-        res = request('GET', apiURL + '/rp/api/lines/' + id + '/direction/1');
-        res = JSON.parse(res.getBody('utf8'));
-    } catch (err) {
-        blockWait();
-        res = request('GET', apiURL + '/rp/api/lines/' + id + '/direction/1');
-        res = JSON.parse(res.getBody('utf8'));
+    repeat = true;
+
+    while (repeat) {
+        repeat = false;
+        try {
+            res = request('GET', apiURL + '/rp/api/lines/' + id + '/direction/1');
+            res = JSON.parse(res.getBody('utf8'));
+        } catch (err) {
+            blockWait();
+            repeat = true;
+        }
     }
 
     addShapes(res.segment_path, id + "1");
@@ -206,7 +230,8 @@ function processLine(id) {
 
 gdata = request('GET', apiURL + '/rp/api/lines/');
 gdata = JSON.parse(gdata.getBody('utf8'));
-for (var i = 0; i < gdata.lines.length; i++) {
+
+for (let i = 0; i < gdata.lines.length; i++) {
     if (!agencyInList(gdata.lines[i].organization.id)) {
         console.log("Adding agency from API, ID: " + gdata.lines[i].organization.id);
         data.agency.push(gdata.lines[i].organization.id.toString());
@@ -215,22 +240,22 @@ for (var i = 0; i < gdata.lines.length; i++) {
 }
 
 function msToHMS(ms) {
-    var seconds = ms / 1000;
-    var hours = parseInt(seconds / 3600);
+    let seconds = ms / 1000;
+    const hours = parseInt(seconds / 3600);
     seconds = seconds % 3600;
-    var minutes = parseInt(seconds / 60);
+    const minutes = parseInt(seconds / 60);
     seconds = seconds % 60;
     return (hours + ":" + minutes);
 }
 
-dirn = start.getTime();
+dirn = `${start.getTime()} (București-${currentService})`;
 
 if (!fs.existsSync("./output/" + dirn)) {
     console.log("Creating output subdirectory " + dirn);
     fs.mkdirSync("./output/" + dirn);
 }
 
-for (property in data) {
+for (let property in data) {
     fs.writeFileSync("./output/" + dirn + "/" + property + ".txt", data[property].join("\r\n"));
 }
 
